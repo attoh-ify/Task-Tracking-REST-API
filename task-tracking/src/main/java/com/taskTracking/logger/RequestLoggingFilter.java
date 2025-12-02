3f15dd7dc04c00c57909283e7bdae91609e89313
Attoh Ifeanyi <alexander.attoh22@gmail.com>
2025-12-02 10:56:12 +0100

    feat: added WebFilter tag to RequestLoggingFilter

--- task-tracking/src/main/java/com/taskTracking/logger/RequestLoggingFilter.java
+++ task-tracking/src/main/java/com/taskTracking/logger/RequestLoggingFilter.java
@@ -3,8 +3,10 @@ package com.taskTracking.logger;
 import javax.servlet.*;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
+@WebFilter("/*")
 public class RequestLoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
